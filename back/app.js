8df6b5964b538969f56edc1f218a39349ac973cf
Julien Helias <julien.helias@free.fr>
2023-02-07 15:25:45 +0100

    Installation de Helmet et MongoSanitize pour la sécurité du back-end

--- back/app.js
+++ back/app.js
@@ -5,11 +5,17 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const cors = require('cors');
+const helmet = require("helmet");
+//const mongoSanitize = require("express-mongo-sanitize");
 
 // Chargement des variables d'environnement 
 
 const dotenv = require('dotenv');
 dotenv.config();
+
+// On protège le back-end de certaines vulnerabilités en protégeant les en-têtes HTTP via HELMET
+
+app.use(helmet());
 
 // Utilisation par app des modules Cors et Express
 
@@ -42,6 +48,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+/*// On purge les données user pour éviter des injections malveillantes dans la BDD via Mongo Sanitize
+
+app.use(mongoSanitize());*/
 
 // Gestion des ressources images par Express de manière statique
 
